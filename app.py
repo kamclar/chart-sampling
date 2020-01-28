29a9ad02ae9c3bcef38950c170d87944471495e1
kamclar <36371300+kamclar@users.noreply.github.com>
2020-01-28 11:23:59 +0100

    add signifikance test

--- app.py
+++ app.py
@@ -8,6 +8,8 @@ at your command prompt. Then navigate to the URL
     http://localhost:5006/sliders
 in your browser.
 '''
+
+from scipy import stats
 from math import sqrt
 import numpy as np
 from statistics import mean, stdev
@@ -36,10 +38,13 @@ for i in range(5):
 
 mean_array = []
 sem_array = []
+samples = []
 x, y = [], []
 x_bar, y_bar = [], []
+t_tests = []
 for i in range(5):
     sampling = np.random.choice(s, N_array[i])
+    samples.append(sampling)
     mean_array.append(mean(sampling))
     sem_array.append(stdev(sampling)/sqrt(N_array[i]))
     x_bar.append(i+1)
@@ -47,8 +52,26 @@ for i in range(5):
     x.append([i + 1, i + 1])
     y.append([mean_array[i], mean_array[i] + sem_array[i]])
 
+signif = []
+signif_mww = []
+for i in range(4):
+    _, res = stats.ttest_ind(samples[i], samples[4])
+    _, res_mww = stats.mannwhitneyu(samples[i], samples[4])
+    t_tests.append(res)
+    if res < 0.05:
+        signif.append(3.5)
+    else:
+        signif.append(-1)
+    if res_mww < 0.05:
+        signif_mww.append(4.5)
+    else:
+        signif_mww.append(-1)
+
+
 source = ColumnDataSource(data=dict(x=x, y=y))
 source_bars = ColumnDataSource(data=dict(x=x_bar, y=y_bar))
+source_signif = ColumnDataSource(data=dict(x=[1, 2, 3, 4], y=signif))
+source_signif_mww = ColumnDataSource(data=dict(x=[1, 2, 3, 4], y=signif_mww))
 print(x)
 print(y)
 
@@ -62,6 +85,8 @@ plot = figure(plot_height=400, plot_width=400, title="Sampling from one populati
 plot.multi_line('x', 'y', source=source,
                line_dash='solid', line_width=2, line_color='black')
 plot.vbar(x='x', top='y', source=source_bars, width=0.5, bottom=0, color="#CAB2D6")
+plot.circle('x', 'y', source=source_signif, size=10, color='navy', alpha=0.5)
+plot.circle('x', 'y', source=source_signif_mww, size=10, color='red', alpha=0.5)
 hline = Span(location=mean(s), dimension='width', line_color='black', line_width=1)
 
 plot.renderers.extend([hline])
@@ -85,7 +110,7 @@ def update_data(attrname, old, new):
     for i in range(5):
         if N_array[i] != no_samples[i].value:
             sampling = np.random.choice(s, no_samples[i].value)
-
+            samples[i] = sampling
             mean_array[i] = (mean(sampling))
             sem_array[i] = (stdev(sampling)/sqrt(N_array[i]))
             N_array[i] = no_samples[i].value
@@ -95,8 +120,24 @@ def update_data(attrname, old, new):
             x_bar[i] = (i + 1)
             y_bar[i] = (mean_array[i])
 
+            if i < 4:
+                _, res = stats.ttest_ind(samples[i], samples[4])
+                _, res_mww = stats.mannwhitneyu(samples[i], samples[4])
+                t_tests[i] = res
+                if res < 0.05:
+                    signif[i] = 3.5
+                else:
+                    signif[i] = -1
+                if res_mww < 0.05:
+                    signif_mww[i] = 4.5
+                    print(res_mww)
+                else:
+                    signif_mww[i] = -1
+
             source.data = dict(x=x, y=y)
             source_bars.data = dict(x=x_bar, y=y_bar)
+            source_signif.data = dict(x=[1, 2, 3, 4], y=signif)
+            source_signif_mww.data = dict(x=[1, 2, 3, 4], y=signif_mww)
 
 
 for w in no_samples:
